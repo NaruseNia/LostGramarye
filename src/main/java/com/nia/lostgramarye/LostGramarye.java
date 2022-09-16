004065fe02193d47ca650b734b3e1c9378ce5354
NaruseNia <notshinsin0000@gmail.com>
2022-09-16 18:33:46 +0900

    Initial Commit

---
+++ src/main/java/com/nia/lostgramarye/LostGramarye.java
@@ -0,0 +1,42 @@
+package com.nia.lostgramarye;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.model.BookModel;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod("lost_gramarye")
+public class LostGramarye {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public LostGramarye() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event) {
+    }
+
+    private void processIMC(final InterModProcessEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+}
